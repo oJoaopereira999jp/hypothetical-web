7418bc0087fbbe257680984fbe684b77b822f632
oJoaopereira999jp <115178299+oJoaopereira999jp@users.noreply.github.com>
2022-11-10 16:43:10 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var getslide = $('.main-box li').length - 1;
+
+    var slidecal = 30/getslide+'%';
+
+    $('.box').css({"width": slidecal});
+
+    $('.box').click(function(){
+        $('.box').removeClass('active');
+       $(this).addClass('active');
+    });
